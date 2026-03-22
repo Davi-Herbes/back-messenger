05a36d7665636387a086752fc3cade4e4dfcfbe4
Davi Herbes <hmatosdavi@gmail.com>
2026-03-22 11:35:33 -0300

    reconfigurando nodemailer

--- src/mail/mailer_service.ts
+++ src/mail/mailer_service.ts
@@ -28,11 +28,16 @@ export class MailService {
   }
 
   async sendUserConfirmation(username: string, email: string, code: string) {
-    await this.transporter.sendMail({
-      from: '"Simple Messenger" d47701206@gmail.com',
-      to: email,
-      subject: "Confirme seu email",
-      html: `<div class="container"><h1>Olá, ${username} o código para autenticação é: </h1><br><h2>${code}</h2></div>`,
-    });
+    try {
+      await this.transporter.sendMail({
+        from: '"Simple Messenger" d47701206@gmail.com',
+        to: email,
+        subject: "Confirme seu email",
+        html: `<div class="container"><h1>Olá, ${username} o código para autenticação é: </h1><br><h2>${code}</h2></div>`,
+      });
+    } catch (e) {
+      console.error("------ERRO DO EMAIL-------", e);
+      throw e;
+    }
   }
 }
